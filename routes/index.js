c06ea505988476c91a8ab3faf003c30fad7d4017
Vitalii Melnykov <vitalii.melnykov@ironsrc.com>
2017-12-05 11:53:13 +0200

    add editorconfig cange URL path for api

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ function init(app) {
   const router = new Router();
 
   router.get('/', require('../controllers/api').version);
-  router.use('/notes', restRouter(require('../controllers/notes')));
+  router.use('/api/notes', restRouter(require('../controllers/notes')));
 
   app
     .use(router.routes())
