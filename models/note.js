5f785a924f6f3c635e2afef34f39ecbd1fbe4bf7
Vitalii Melnykov <fsoulmonk@gmail.com>
2018-04-20 18:07:29 +0300

    note schema update

--- models/note.js
+++ models/note.js
@@ -4,14 +4,30 @@ const mongoose = require('mongoose');
 
 const NoteSchema = new mongoose.Schema({
 
-  title: String,
-  keywords: [String],
+  title: {
+    type: String,
+    required: true
+  },
 
   text: {
     type: String,
     required: true
   },
 
+  description: String,
+  keywords: [String],
+
+  // category
+});
+
+NoteSchema.method('toJSON', function () {
+  const obj = this.toObject();
+
+  //Rename fields
+  obj.id = obj._id;
+  delete obj._id;
+
+  return obj;
 });
 
 module.exports = mongoose.model('Note', NoteSchema);
